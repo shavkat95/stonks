2b2fc28d7d1d9483c7bc7bbb600a09caf81b9f07
shavkat95 <shavkat95@icloud.com>
2024-06-08 22:14:14 +0200

    m

--- scrape_for_btc.py
+++ scrape_for_btc.py
@@ -10,8 +10,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 import selenium
 from selenium.webdriver.common.by import By
 from selenium.webdriver import EdgeOptions
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 
