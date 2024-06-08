8a67ee2d9f7ed0dbb6a91552f9532d52f8b2c1ad
shavkat95 <shavkat95@icloud.com>
2024-06-08 21:10:05 +0200

    m

--- scrape_for_btc.py
+++ scrape_for_btc.py
@@ -102,7 +102,7 @@ def get_search_data(kw):
 
 
 def scrape_reddit_btc():
-    driver = webdriver.Edge(options=options)
+    driver = webdriver.Edge(options=options, executable_path="path/to/edgedriver_linux64/msedgedriver")
     result = ''
     driver.get('https://www.reddit.com/search/?q=bitcoin&sort=new')
 
