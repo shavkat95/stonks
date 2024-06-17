99f6507e92dfac66fac2a3ef1e9ed0356a172a34
shavkat95 <shavkat95@icloud.com>
2024-06-17 23:13:45 +0200

    bugfix

--- the_do.py
+++ the_do.py
@@ -266,7 +266,7 @@ def scrape_post(context, url):
     except:
         print('ups 1')
         try:
-            time.sleep(10)
+            time.sleep(1)
             pages = context.pages
             for i in range(1, len(pages)):
                 pages[i].close()
@@ -374,8 +374,6 @@ def one_search_page(context, page):
             if len(pages) > 1:
                 for i in range(1, len(pages)):
                     pages[i].close()
-            time.sleep(1)
-            # page.reload()
             time.sleep(1)
             scroll_to_bottom(page)
             time.sleep(1)
